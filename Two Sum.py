e04bc59185b54b541dbf3e2be4d5ab09d063fa6e
James Jeberson <160733455+JamesJeberson@users.noreply.github.com>
2025-06-05 07:42:02 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -28,6 +28,8 @@
 
 ###############################################################################################################################
 
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Brute Force
