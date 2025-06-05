87d123f31c549bc840647d8bee37b823ee1c195b
James Jeberson <160733455+JamesJeberson@users.noreply.github.com>
2025-06-05 07:40:35 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,10 +1,6 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 # You can return the answer in any order.
-
- 
 
 # Example 1:
 
@@ -20,7 +16,6 @@
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
  
-
 # Constraints:
 
 # 2 <= nums.length <= 104
@@ -31,7 +26,7 @@
 
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
-
+###############################################################################################################################
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
